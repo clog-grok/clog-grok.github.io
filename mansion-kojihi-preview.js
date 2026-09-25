2aa785f77ec224a52dcf866f060dadf2248d73d9
clog-grok <connectorlog@gmail.com>
2026-09-25 15:33:47 +0900

    preview: JS loader cache-bust v=22-typefold

--- mansion-kojihi-preview.js
+++ mansion-kojihi-preview.js
@@ -1,7 +1,7 @@
 (async function () {
   var n = 8;
   var parts = await Promise.all(Array.from({length: n}, function (_, i) {
-    return fetch("./mansion-kojihi-preview.h" + i + ".b64?v=21-coeff").then(function (r) { return r.text(); });
+    return fetch("./mansion-kojihi-preview.h" + i + ".b64?v=22-typefold").then(function (r) { return r.text(); });
   }));
   var bin = atob(parts.join(""));
   var bytes = new Uint8Array(bin.length);
