55ac22fd4a1d53c6153f31bef7c8ac8aec896d3e
clog-grok <connectorlog@gmail.com>
2026-09-25 15:09:15 +0900

    preview: b64 JS loader for coeff edit

--- mansion-kojihi-preview.js
+++ mansion-kojihi-preview.js
@@ -1,7 +1,10 @@
 (async function () {
   var n = 4;
   var parts = await Promise.all(Array.from({length: n}, function (_, i) {
-    return fetch("./mansion-kojihi-preview.p" + i + ".js?v=21-coeff").then(function (r) { return r.text(); });
+    return fetch("./mansion-kojihi-preview.p" + i + ".b64?v=21-coeff").then(function (r) { return r.text(); });
   }));
-  (0, eval)(parts.join(""));
+  var bin = atob(parts.join(""));
+  var bytes = new Uint8Array(bin.length);
+  for (var i = 0; i < bin.length; i++) bytes[i] = bin.charCodeAt(i);
+  (0, eval)(new TextDecoder("utf-8").decode(bytes));
 })();
