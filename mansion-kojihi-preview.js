235e170349a6ef669997df93eb1e65995566349a
clog-grok <connectorlog@gmail.com>
2026-09-25 15:07:41 +0900

    preview: JS loader for coeff parts (temp)

--- mansion-kojihi-preview.js
+++ mansion-kojihi-preview.js
@@ -1,1 +1,7 @@
-LOADING_FROM_FILE
+(async function () {
+  var n = 4;
+  var parts = await Promise.all(Array.from({length: n}, function (_, i) {
+    return fetch("./mansion-kojihi-preview.p" + i + ".js?v=21-coeff").then(function (r) { return r.text(); });
+  }));
+  (0, eval)(parts.join(""));
+})();
